78e6f8deec1864f871bf1e781a74bcdb36ee006b
Anwesh43 <anweshthecool0@gmail.com>
2022-09-12 16:49:10 +0530

    useStyle hook

--- src/hooks.ts
+++ src/hooks.ts
@@ -1,7 +1,8 @@
-import {useState, useEffect} from 'react'
+import {useState, useEffect, CSSProperties} from 'react'
 
 const delay : number = 20 
 const scGap : number = 0.02 
+const sizeFactor : number = 6.9 
 
 export const useAnimatedScale = () => {
     const [animated, setAnimated] = useState(false)
@@ -35,5 +36,44 @@ export const useDimension = () => {
     }, [])
     return {
         w, h
+    }
+}
+
+export const useStyle = (w : number, h : number, scale : number) => {
+    
+    const background = 'indigo'
+    const position = 'absolute'
+    const size : number = Math.min(w, h) / sizeFactor
+    const barH : number = size / 3 
+    const sf = Math.sin(scale * Math.PI)
+    return {
+        barStyle()  : CSSProperties {
+            const width = `${size - size * sf}px`
+            const height = `${barH}px`
+            const left = `${w / 2 - size / 2}px`
+            const top = `${h / 2 - barH / 2}px`
+            return {
+                position,
+                width, 
+                height, 
+                background,
+                borderRadius: '4px',
+                left, 
+                top 
+            }
+        },
+        lineStyle() : CSSProperties {
+            const left = `${w * 0.75}px`
+            const top = `${-size + (h / 2) * sf}px`
+            const height = `${size}px`
+            const width = `${Math.min(w, h) / 90}px`
+            return {
+                position, 
+                left, 
+                top, 
+                height,
+                width 
+            }
+        }
     }
 }
